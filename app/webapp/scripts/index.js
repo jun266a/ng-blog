822c7c36ed66de7de16df4a261e1f83a0eda20cb
jun266a <jun266a@foxmail.com>
2018-07-19 14:32:38 +0800

    监听用户登录状态，防止未授权访问

--- app/webapp/scripts/index.js
+++ app/webapp/scripts/index.js
@@ -7,6 +7,21 @@
 	]);
 	app.config(['$routeProvider',function($routeProvider){
 		$routeProvider
-		.otherwise({redirectTo : '/user'});
+		.otherwise({redirectTo : '/user/login'});
+	}]);
+	app.run(['$rootScope','$location',function($rootScope,$location){
+		// 防止FQ
+		$rootScope.$on('$routeChangeStart',function(){
+			//检查登录标记，过滤所有路径
+			var UID = window.sessionStorage.getItem('UID');
+			//广播
+			$rootScope.$broadcast('UID', UID);
+			//登录或者注册页面不过滤
+			var USER = $location.path().startsWith('/user');
+			if(!UID&&!USER){
+				//强制跳转到登录或者注册页面
+				$location.path('/user/login')
+			}
+		});
 	}]);
 })(angular);
